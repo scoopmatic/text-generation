44acaa3fee29e568c810e6f32957ee844542961c
Samuel Rönnqivst <samuel.ronnqvist@gmail.com>
2018-07-31 13:27:47 +0300

    major commit

--- model.py
+++ model.py
@@ -1,85 +1,265 @@
 from keras.models import Model, model_from_json
 from keras.layers import Dense, Input, Reshape, Flatten, concatenate, Bidirectional, TimeDistributed, RepeatVector, Dropout
-#from keras.layers.recurrent import CuDNNLSTM as LSTM
-from keras.layers.recurrent import LSTM
+from keras.layers import CuDNNLSTM as LSTM
+#from keras.layers.recurrent import LSTM
 from keras.layers.embeddings import Embedding
 from keras import optimizers
+from keras.preprocessing import sequence
 
 import json
 import sys
 import numpy as np
+import random
+import heapq
 
+alpha = 1.1
+#len_norm = (lambda x: x[0]/((5+len(x[1]))**alpha/(5+1)**alpha))
+len_norm = (lambda x: x[0]/len(x[1])**alpha)
+vocab_norm = (lambda x: x[0]/len(set(x[1]))**alpha)
 
 from keras.callbacks import Callback
 
 def sample(preds, temperature=1.0):
-    # helper function to sample an index from a probability array
-    preds = np.asarray(preds).astype('float64')
-    preds = np.log(preds) / temperature
-    exp_preds = np.exp(preds)
-    preds = exp_preds / np.sum(exp_preds)
-    probas = np.random.multinomial(1, preds, 1)
-    return np.argmax(probas)
+	# helper function to sample an index from a probability array
+	preds = np.asarray(preds).astype('float64')
+	preds = np.log(preds) / temperature
+	exp_preds = np.exp(preds)
+	preds = exp_preds / np.sum(exp_preds)
+	probas = np.random.multinomial(1, preds, 1)
+	return np.argmax(probas)
+
+
+def input_sequence(sample, seq_len):
+	sequence = np.zeros((1,seq_len)) # <unk>s
+	sequence[0,0:len(sample)] = sample
+	return sequence
+
+
+def mrbeam(state, decoder, k=3, max_len=100, seq_len=400, sp_model=None):
+	""" Beam search text generator """
+	UNK, BOS, EOS = 0, 1, 2
+	dead_samples = []
+	dead_scores = []
+	live_samples = [[BOS]]
+	live_scores = [0]
+	for i in range(min(seq_len, max_len)):
+		print("\ri: %d, alive: %d, dead: %d   " % (i, len(live_samples), len(dead_samples)), end="")
+		live_sequences = np.concatenate([input_sequence(sample, seq_len) for sample in live_samples])
+		rep = live_sequences.shape[0]
+		state_ = [np.repeat(state[0],rep).reshape((rep,state[0].shape[1])), np.repeat(state[0],rep).reshape((rep,state[0].shape[1]))]
+		pred, _, _ = decoder.predict([live_sequences] + state_)
+
+		tops = [np.argsort(pred[n][i])[-k:] for n in range(pred.shape[0])]
+		probs = [[pred[n][i][j] for j in tops[n]] for n in range(pred.shape[0])]
+
+		spare_samples, spare_scores = [], []
+		#print()
+		for n, prob_dist in enumerate(probs):
+			#print(n, sp_model.DecodeIds(live_samples[n]), ':', ' '.join([sp_model.IdToPiece(int(id))+("(%.3f)"%probs[n][i]) for i, id in enumerate(tops[n])]))
+			"""
+			# Experimental: multinomial sampling over token probability distribution
+			exp_probs = np.exp(np.log(prob_dist) / 0.6)
+			selected = False
+			while not selected:
+				picks = [np.argmax(np.random.multinomial(1, exp_probs / np.sum(exp_probs), 1)) for x in range(10)]
+				picks = [(p, prob_dist[p]) for p in set(picks)]
+				for m, token_prob in picks:"""
+			for m, token_prob in enumerate(prob_dist):
+				samples = live_samples[n] + [int(tops[n][m])]
+				scores = live_scores[n] - np.log(token_prob)
+				if tops[n][m] == EOS:
+					dead_samples.append(samples)
+					dead_scores.append(scores)
+					#selected = True
+				elif tops[n][m] == UNK:
+					pass
+					#spare_samples.append(live_samples[n])
+					#spare_scores.append(live_scores[n])
+				else:
+					spare_samples.append(samples)
+					spare_scores.append(scores)
+					#selected = True
+
+		try:
+			live_scores, live_samples = zip(*heapq.nsmallest(k, zip(spare_scores, spare_samples), key=vocab_norm))
+		except:
+			import pdb; pdb.set_trace()
+		if len(live_samples) == 0:
+			break
+	print()
+	return sorted(zip(dead_scores + list(live_scores), dead_samples + list(live_samples)), key=vocab_norm, reverse=True)
 
 
 class GenerationCallback(Callback):
 
-    def __init__(self, vocabulary):
+	def __init__(self, vocabulary, seq_len, models, sp_model=None, generator=None):
 
-        self.vocabulary=vocabulary
-        self.inversed_vocabulary={value:key for key, value in vocabulary.items()}
+		self.vocabulary=vocabulary
+		self.inverse_vocabulary={value:key for key, value in vocabulary.items()}
+		self.model, self.encoder_model, self.decoder_model = models
+		self.seq_len = seq_len
+		self.sp_model = sp_model
+		self.generator = generator
 
-    def on_epoch_end(self, epoch, logs={}):
+	def on_epoch_end(self, epoch, logs={}):
+		vocab = self.vocabulary
+		inv_vocab = self.inverse_vocabulary
+		try:
+			input, output = next(self.generator)
+			encoder_in, decoder_in = input
+		except ValueError as err:
+			print(err)
+			return
 
-        pass
-        
-       
+		state = self.encoder_model.predict(encoder_in[0:1,:])
+		print(self.sp_model.DecodeIds([int(x) for x in encoder_in[0,] if x > 0]))
+		beams = mrbeam(state, self.decoder_model, k=30, max_len=epoch//4+20, seq_len=self.seq_len, sp_model=self.sp_model)
+		for n, (score, sample) in enumerate(beams):
+			if n % (len(beams)//8) == 0 or n >= len(beams)-5:
+				print("%d --> %s (%.3f)" % (n, self.sp_model.DecodeIds([int(x) for x in sample]), vocab_norm((score, sample))))
 
+		"""
+		# Multinomial sampling over token probability distribution
+		for diversity in [0.2, 0.5]:
+			unk_cnt = 0
+			print("Diversity: %.1f" % diversity)
+			generated = []
+			#sys.stdout.write(" ".join(generated))
+			generate_X = np.zeros((1,self.seq_len))
+			generate_X[:,0] = self.sp_model.PieceToId('<s>')
+			for i in range(0, 100):
+				# predict
+				#preds = self.model.predict([doc_id, generate_X], verbose=0)[0]
+				#preds = self.model.predict(generate_X, verbose=0)[0]
+				preds = self.decoder_model.predict([generate_X] + state, verbose=0)[0]
+				next_index = sample(preds[0,i,:], diversity)
+				next_char = inv_vocab[next_index]
+				generate_X[:,i+1] = next_index
+				#generate_X = np.array([np.append(generate_X, [next_index])])
 
+				#sentence=generated[len(generated)-context_size:]
+
+				# vectorize new seed
+				#generate_X=np.zeros((1,context_size))
+				#for i,c in enumerate(sentence):
+				#    generate_X[0,i]=vocab.get(c,vocab["<UNKNOWN>"])
+
+				sys.stdout.write(' ')
+				sys.stdout.write(self.sp_model.IdToPiece(int(next_index)))
+				if next_index in [self.sp_model.PieceToId('</s>'), self.sp_model.PieceToId('<unk>')]:
+					out = self.sp_model.IdToPiece(int(next_index))
+					##print(out, end="")
+					unk_cnt += 1
+					if unk_cnt > 3 or out == '</s>':
+						print()
+						break
+
+				sys.stdout.flush()
+
+			sys.stdout.write("\n")
+			sys.stdout.flush()
+			print("%s\n --> %s" % (self.sp_model.DecodeIds([int(x) for x in encoder_in[0,] if x > 0]),
+								self.sp_model.DecodeIds([int(x) for x in generate_X[0,:i]])))
+		"""
 
 class GenerationModel(object):
 
-    def __init__(self, vocab_size, sequence_len, args):
-        """args: parameters from argparse"""
+	def __init__(self, vocab_size, sequence_len, args):
+		"""args: parameters from argparse"""
 
-        print("Building model",file=sys.stderr)
+		print("Building model",file=sys.stderr)
+		## Encoder/decoder embeddings
+		shared_embeddings = Embedding(vocab_size, args.embedding_size, name="embeddings")
 
-        # inputs
-        input_=Input(shape=(sequence_len,))
-        embeddings=Embedding(vocab_size, args.embedding_size)(input_)
-        lstm=LSTM(args.recurrent_size, return_sequences=True)(embeddings)
-        drop=Dropout(args.dropout)(lstm)
-        hidden=TimeDistributed(Dense(args.hidden_dim, activation="tanh"))(drop)
-        classification=TimeDistributed(Dense(vocab_size, activation="softmax"))(hidden)
+		## Encoder model
+		encoder_input = Input(shape=(sequence_len,))
+		encoder_emb = shared_embeddings(encoder_input)
 
-        self.model=Model(inputs=[input_], outputs=[classification])
+		encoder_lstm1 = LSTM(args.recurrent_size, return_sequences=False, return_state=True, name="encoder_lstm1")(encoder_emb)
+		encoder_outputs, encoder_state_h, encoder_state_c = encoder_lstm1
+		encoder_outputs = Dropout(args.dropout)(encoder_outputs)
+		#encoder_outputs, encoder_state_h, encoder_state_c = encoder_drop1
+		encoder_states = [encoder_state_h, encoder_state_c]
 
-        optimizer = optimizers.Adam(lr=args.learning_rate)
-        self.model.compile(loss="sparse_categorical_crossentropy", optimizer=optimizer)
+		## Decoder model (for training)
+		decoder_input = Input(shape=(sequence_len,)) # Gold standard input for training
+		decoder_emb = shared_embeddings(decoder_input)
+		# Embeddings and state as input (peeky)
+		encoder_state_h_rep = RepeatVector(sequence_len)(encoder_state_h)
+		encoder_state_c_rep = RepeatVector(sequence_len)(encoder_state_c)
+		decoder_merged_input = concatenate([decoder_emb, encoder_state_h_rep, encoder_state_c_rep])
 
-        print(self.model.summary())
-        
-    
-    
-    
+		"""# Begin decoder LSTM block
+		decoder_lstm1 = LSTM(args.recurrent_size, return_sequences=True, name="decoder_lstm1")
+		decoder_drop1 = Dropout(args.dropout)(decoder_lstm1)
+		decoder_lstm2 = LSTM(args.recurrent_size, return_sequences=True, return_state=True, name="decoder_lstm2")(decoder_drop1, initial_state=encoder_states)
+		decoder_lstm_block = Dropout(args.dropout)(decoder_lstm2) # End block"""
+
+		decoder_lstm1 = LSTM(args.recurrent_size, return_sequences=True, return_state=True, name="decoder_lstm1")
+		decoder_lstm2 = LSTM(args.recurrent_size, return_sequences=True, return_state=True, name="decoder_lstm2")
+		hidden=TimeDistributed(Dense(args.hidden_dim, activation="tanh"))
+		decoder_softmax = TimeDistributed(Dense(vocab_size, activation="softmax", name="decoder_out"))
+		#decoder_output, _, _ = decoder_lstm_block(decoder_emb, initial_state=encoder_states)
+
+		decoder_lstm1_output, _, _ = decoder_lstm1(decoder_merged_input, initial_state=encoder_states)
+		#decoder_lstm1_output, _, _ = decoder_lstm1(decoder_emb, initial_state=encoder_states)
+
+		decoder_lstm1_drop = Dropout(args.dropout)(decoder_lstm1_output)
+		#decoder_lstm2_output, _, _ = decoder_lstm2(decoder_lstm1_drop, initial_state=encoder_states)
+		#decoder_lstm2_drop = Dropout(args.dropout)(decoder_lstm2_output)
+		decoder_output = hidden(decoder_lstm1_drop)
+		decoder_output = Dropout(args.dropout)(decoder_output)
+		decoder_output = decoder_softmax(decoder_output) #decoder_output = decoder_softmax(decoder_lstm2_drop)
+
+		self.model = Model(inputs=[encoder_input, decoder_input], outputs=[decoder_output])
+		self.encoder_model = Model(encoder_input, encoder_states) # Stand-alone encoder
+
+		## Stand-Alone decoder model (for generation)
+		SA_decoder_state_input_h = Input(shape=(args.recurrent_size,))
+		SA_decoder_state_input_c = Input(shape=(args.recurrent_size,))
+		SA_decoder_state_input = [SA_decoder_state_input_h, SA_decoder_state_input_c]
+
+		# State as input variable (as well as initial state), merged with embeddings (of generated sequence)
+		SA_decoder_state_input_h_rep = RepeatVector(sequence_len)(SA_decoder_state_input_h)
+		SA_decoder_state_input_c_rep = RepeatVector(sequence_len)(SA_decoder_state_input_c)
+		SA_decoder_merged_input = concatenate([decoder_emb, SA_decoder_state_input_h_rep, SA_decoder_state_input_c_rep])
+
+		#decoder_output, state_h, state_c = decoder_lstm_block(decoder_emb, initial_state=decoder_state_input)
+
+		SA_decoder_lstm1_output, state_h, state_c = decoder_lstm1(SA_decoder_merged_input, initial_state=SA_decoder_state_input)
+		##SA_decoder_lstm1_output, _, _ = decoder_lstm1(SA_decoder_merged_input, initial_state=SA_decoder_state_input)
+		#SA_decoder_lstm1_output, _, _ = decoder_lstm1(decoder_emb, initial_state=SA_decoder_state_input)
+
+		SA_decoder_lstm1_drop = Dropout(args.dropout)(SA_decoder_lstm1_output)
+		#SA_decoder_lstm2_output, state_h, state_c = decoder_lstm2(SA_decoder_lstm1_drop, initial_state=SA_decoder_state_input)
+		#SA_decoder_lstm2_drop = Dropout(args.dropout)(SA_decoder_lstm2_output)
+		SA_decoder_output = hidden(SA_decoder_lstm1_drop)
+		SA_decoder_output = Dropout(args.dropout)(SA_decoder_output)
+		SA_decoder_output = decoder_softmax(SA_decoder_output)
+		#SA_decoder_output = decoder_softmax(SA_decoder_lstm2_drop)
+		SA_decoder_states = [state_h, state_c]
+		#decoder_output = decoder_softmax(decoder_output)
+		self.decoder_model = Model([decoder_input] + SA_decoder_state_input, [SA_decoder_output] + SA_decoder_states)
+
+		optimizer = optimizers.Adam(lr=args.learning_rate, amsgrad=True)
+		self.model.compile(loss="sparse_categorical_crossentropy", optimizer=optimizer)
+
+		print(self.model.summary())
+
+
 def load_model(model_fname, weight_fname):
+	with open(model_fname, "rt", encoding="utf-8") as f:
+		model=model_from_json(f.read())
+	model.load_weights(weight_fname)
 
-    with open(model_fname, "rt", encoding="utf-8") as f:
-        model=model_from_json(f.read())
-    model.load_weights(weight_fname)
-
-    return model
+	return model
 
 def save_model(model, model_fname, weight_fname="None", save_weights=False):
-
-    model_json = model.to_json()
-    with open(model_fname, "w") as f:
-        print(model_json,file=f)
-    if save_weights:
-        model.save_weights(weight_fname)
+	model_json = model.to_json()
+	with open(model_fname, "w") as f:
+		print(model_json,file=f)
+	if save_weights:
+		model.save_weights(weight_fname)
 
 if __name__=="__main__":
-
-    pass
-
-
+	pass
--- train.py
+++ train.py
@@ -6,8 +6,11 @@ import numpy as np
 import math
 from keras.preprocessing.sequence import pad_sequences
 from model import GenerationModel, save_model, load_model, GenerationCallback
+from keras.callbacks import ModelCheckpoint
+from keras.models import load_model as load_model_
+import sentencepiece as spm
 
-
+DATA_PATH="/home/samuel/data-textgen/all_stt.conllu.gz"
 
 ID,FORM,LEMMA,FEATS,UPOS,XPOS,HEAD,DEPREL,DEPS,MISC=range(10)
 def build_vocabularies(documents):
@@ -19,6 +22,7 @@ def build_vocabularies(documents):
                     char_vocab.setdefault(char,len(char_vocab))
     return char_vocab
 
+
 def vectorize_doc(document,char_vocab):
     doc_char_vectorized=[]
     for comment,sent in document:
@@ -28,8 +32,10 @@ def vectorize_doc(document,char_vocab):
         doc_char_vectorized.append(sent_char_vectorized)
     return doc_char_vectorized
 
+
 def infinite_data_vectorizer(vocabulary, data_file):
     pass
+
 
 def infinite_datareader(fname, max_documents=0):
     document_counter=0
@@ -39,84 +45,143 @@ def infinite_datareader(fname, max_documents=0):
         print("Iteration:", iteration)
         for document, meta in inp.get_documents(fname):
             document_counter+=1
-            yield document, iteration
+            yield document, iteration, document_counter-1
             if max_documents!=0 and document_counter>=max_documents:
                 break
 
-def infinite_vectorizer(vocabulary, fname, batch_size, sequence_len):
+def infinite_vectorizer(vocabulary, fname, batch_size, sequence_len, sp_model=None):
     """ ... """
-    data=[]
-    for document, iteration in infinite_datareader(fname):
+
+    inputs=np.zeros((batch_size, sequence_len)) # 0==<unk> (padding)
+    outputs=np.zeros((batch_size, sequence_len))
+    batch_i = 0
+    for document, iteration, doc_id in infinite_datareader(fname):
+        # Warning: doc_id is a counter that doesn't reset at next iteration
         # vectorize_doc: document is a list of sentences, sentence is a list of words
         #  ... and word is a list of characters
         # --> flatten this to get a document as a list of characters, and add an end-of-word
         #  ... markers to represent white space
-        vectorized_document=[vocabulary["<BOD>"]]
-        for sent in vectorize_doc(document, vocabulary):
-            for word in sent:
-                for char in word:
-                    vectorized_document.append(char)
-                vectorized_document.append(vocabulary["<EOW>"])
-        vectorized_document.append(vocabulary["<EOD>"])
-        data+=vectorized_document
-        if len(data)>batch_size*sequence_len:
-            batch_input=np.array(data[:batch_size*sequence_len]).reshape(batch_size, sequence_len) # TODO do not remove the last (uncomplete) row
-            # targets: shift by one character
-            batch_output=np.array(data[1:batch_size*sequence_len+1]).reshape(batch_size, sequence_len)
-            yield (batch_input, np.expand_dims(batch_output,-1))
-            data=[]
-        
- 
+
+        doc = []
+        for comment,sent in document:
+            sent_str = ' '.join([cols[FORM] for cols in sent])
+            sent_str =  sent_str.replace(' .', '.')\
+                                .replace(' ,', ',')\
+                                .replace(' :', ':')
+            doc.append(sent_str)
+
+        doc_str = ' '.join(doc)
+        #doc_pcs = sp.EncodeAsPieces(doc_str)
+        doc_pc_ids = [sp_model.PieceToId('<s>')]+sp_model.EncodeAsIds(doc_str)+[sp_model.PieceToId('</s>')]
+
+        #th = np.random.random()*0.5*(1-doc_id%10000/10000)+0.2
+        ##th = 1-doc_id/2000000
+        ##hint_ids = sp_model.EncodeAsIds(' '.join([w for w in doc if th>np.random.random()][:50]))
+
+        ##print(" doc id: %d (%d pcs)    " % (doc_id, len(doc_pc_ids)), end="")
+        data = doc_pc_ids
+
+        #doc_input = np.zeros((batch_size, 1))
+        #doc_input[:] = doc_id
+
+        while len(data)>sequence_len:
+            inputs[batch_i,:] = data[:sequence_len]
+            outputs[batch_i,:] = data[1:sequence_len+1]
+            data = data[sequence_len:]
+        else:
+            try:
+                inputs[batch_i,-len(data)+1:] = data[:-1]
+            except ValueError:
+                if len(data) == 1: # Skip <s>-only sequence
+                    continue
+                else:
+                    raise
+
+            outputs[batch_i,-len(data)+1:] = data[1:]
+            ##yield ([inputs[:batch_i+1,:], inputs[:batch_i+1,:]], np.expand_dims(outputs[:batch_i+1,:], -1))#np.expand_dims(outputs,-1))
+
+        if batch_i == batch_size-1:
+            yield ([inputs, inputs], np.expand_dims(outputs,-1))
+            inputs=np.zeros((batch_size, sequence_len))
+            outputs=np.zeros((batch_size, sequence_len))
+            batch_i = 0
+        else:
+            batch_i += 1
 
 
-def train(args):
+#if __name__=="__main__":
+import argparse
+parser = argparse.ArgumentParser(description='')
+g=parser.add_argument_group("Reguired arguments")
 
-    # vocabulary
-    if not os.path.exists("char_vocab.json"):
-        docs=inp.get_documents("/home/ginter/text-generation/all_stt.conllu.gz")
-        char_vocab=build_vocabularies(itertools.islice(docs,10000))
-        with open("char_vocab.json","wt") as f:
-            json.dump(char_vocab,f)
-    else:
-        with open("char_vocab.json","rt") as f:
-            char_vocab=json.load(f)
+g.add_argument('--embedding_size', type=int, default=100, help='Character embedding size')
+g.add_argument('--recurrent_size', type=int, default=200, help='Recurrent layer size')
+g.add_argument('--dropout', type=float, default=0.1, help='Dropout rate')
+g.add_argument('--hidden_dim', type=int, default=100, help='Size of the hidden layer in timedistributed')
+g.add_argument('--learning_rate', type=float, default=0.001, help='Learning rate')
 
-    inversed_vocabulary={value:key for key, value in char_vocab.items()}
-
-    batch_size=60
-    sequence_len=101
-    train_vectorizer=infinite_vectorizer(char_vocab, "/home/ginter/text-generation/all_stt.conllu.gz", batch_size, sequence_len)
-
-    example_input, example_output=next(train_vectorizer)
-    print("input:",[inversed_vocabulary[t] for t in example_input[0,:]])
-    print("output:",[inversed_vocabulary[t] for t in example_output.reshape(batch_size,sequence_len)[0,:]])
-
-    generation_model=GenerationModel(len(char_vocab), sequence_len, args)
-
-    generate_callback=GenerationCallback(char_vocab)
-
-    # steps_per_epoch: how many batcher before running callbacks
-    # epochs: how many steps to run, should be basically infinite number (until killed)
-    generation_model.model.fit_generator(train_vectorizer, steps_per_epoch=100, epochs=1000000, verbose=1, validation_data=(example_input,example_output), callbacks=[generate_callback])
-    
+args = parser.parse_args()
+#model = train(args)
 
 
+#def train(args):
+
+sp = spm.SentencePieceProcessor()
+sp.Load("m8k.sentpc")
+sp_vocab = {sp.IdToPiece(i): i for i in range(sp.GetPieceSize())}
+inv_sp_vocab = {value:key for key, value in sp_vocab.items()}
+
+batch_size=10
+sequence_len=100#250#1601
+#n_docs = 1063180
+
+train_vectorizer=infinite_vectorizer(sp_vocab, DATA_PATH, batch_size, sequence_len, sp_model=sp)
+
+examples = [next(train_vectorizer) for i in range(10)]
+example_input = [np.concatenate([inp[0] for inp, outp in examples]), np.concatenate([inp[1] for inp, outp in examples])]
+example_output = np.concatenate([outp for inp, outp in examples])
+
+generation_model=GenerationModel(len(sp_vocab), sequence_len, args)
+
+#from keras.utils import multi_gpu_model
+#para_model=multi_gpu_model(generation_model.model, gpus=4)
+
+generate_callback=GenerationCallback(sp_vocab, sequence_len, [generation_model, generation_model.encoder_model, generation_model.decoder_model], sp_model=sp, generator=train_vectorizer)
+checkpointer = ModelCheckpoint(filepath='seq2seq_weights_6.hdf5', verbose=1, save_best_only=True, save_weights_only=True)
 
 
+#generation_model.model.load_weights('seq2seq_weights_5_flat_batch.hdf5')
 
-if __name__=="__main__":
-    
-    import argparse
+"""
+generation_model.model.layers[2] = tmp_model.model.layers[2]
+# encoder
+#for trg, src in [(1,2),(2,3)]:
+for trg, src in [(1,2)]:
+    generation_model.encoder_model.layers[trg] = tmp_model.model.layers[trg]
 
-    parser = argparse.ArgumentParser(description='')
-    g=parser.add_argument_group("Reguired arguments")
-    
-    g.add_argument('--embedding_size', type=int, default=100, help='Character embedding size')
-    g.add_argument('--recurrent_size', type=int, default=200, help='Recurrent layer size')
-    g.add_argument('--dropout', type=float, default=0.1, help='Dropout rate')
-    g.add_argument('--hidden_dim', type=int, default=100, help='Size of the hidden layer in timedistributed')
-    g.add_argument('--learning_rate', type=float, default=0.001, help='Learning rate')
-    
-    args = parser.parse_args()
-        
-    train(args)
+# decoder
+#for trg, src in [(1,2),(4,4),(6,6),(8,8)]:
+for trg, src in [(1,2)]:
+    generation_model.decoder_model.layers[trg] = tmp_model.model.layers[trg]
+"""
+"""
+for layer in [5, 9, 11]:
+    generation_model.model.layers[layer].set_weights(pretrained_model.layers[layer].get_weights())
+layer = 7
+new_weights = np.zeros((300,1200))+0.0001
+new_weights[100:,:] = pretrained_model.layers[layer].get_weights()[0]
+all_weights = pretrained_model.layers[layer].get_weights()
+all_weights[0] = new_weights
+generation_model.model.layers[layer].set_weights(all_weights)
+"""
+#generation_model.model.layers[2].set_weights([np.load("../neuraltopics/doc2topic/all_stt_docvecs.npy")])
+
+# steps_per_epoch: how many batcher before running callbacks
+# epochs: how many steps to run, should be basically infinite number (until killed)
+#para_model.compile(loss="sparse_categorical_crossentropy", optimizer="adam")
+#para_model.fit_generator(train_vectorizer, steps_per_epoch=2000, epochs=1000000, verbose=1, callbacks=[generate_callback, checkpointer], validation_data=(example_input,example_output))
+
+generation_model.model.fit_generator(train_vectorizer, steps_per_epoch=5000, initial_epoch=0, epochs=2000, verbose=1, callbacks=[generate_callback,checkpointer], validation_data=(example_input,example_output))
+
+
+#return generation_model
